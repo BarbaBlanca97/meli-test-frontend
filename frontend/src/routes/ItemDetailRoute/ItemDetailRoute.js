070b56f6a6c85d52e05ce43810283954eeae3e2f
barbablanca97 <jere.chiosso@gmail.com>
2022-11-21 20:34:09 -0300

    Basic item detail functionality

--- frontend/src/routes/ItemDetailRoute/ItemDetailRoute.js
+++ frontend/src/routes/ItemDetailRoute/ItemDetailRoute.js
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom"
 import ItemsSearchBar from "../../components/SearchBar/ItemSearchBar/ItemsSearchBar"
 
 function ItemDetail() {
+    const { itemId } = useParams()
+
     return <div>
         <ItemsSearchBar />
+        <div>Estamos viendo el item con id: {itemId}</div>
     </div>
 }
 
