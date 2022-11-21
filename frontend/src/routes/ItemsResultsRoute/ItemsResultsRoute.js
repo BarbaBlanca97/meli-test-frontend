c18a370938d482714dd0854add4711aab0dfa233
barbablanca97 <jere.chiosso@gmail.com>
2022-11-21 20:18:42 -0300

    Added items result base functionality

--- frontend/src/routes/ItemsResultsRoute/ItemsResultsRoute.js
+++ frontend/src/routes/ItemsResultsRoute/ItemsResultsRoute.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import ItemsSearchBar from "../../components/SearchBar/ItemSearchBar/ItemsSearchBar"
 
 function ItemsResults() {
+    const [items, setItems] = useState([])
+    const [searchParams] = useSearchParams()
+
+    // replace
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setItems([
+                {
+                    id: 'id1',
+                    name: 'Item 1'
+                },
+                {
+                    id: 'id2',
+                    name: 'Item 2'
+                },
+                {
+                    id: 'id3',
+                    name: 'Item 3'
+                },
+            ])
+        }, 1300)
+
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    })
+
     return <div>
         <ItemsSearchBar />
+        <div>
+            {items.map(item => <div key={item.id}>
+                <div>
+                    Item: {item.name}
+                </div>
+                <Link to={`/items/${item.id}`}>Go to item {item.id}</Link>
+                <br />
+                <br />
+            </div>)}
+        </div>
     </div>
 }
 
