f237a6fc8231d1686532df54f38305bd45e91dfd
barbablanca97 <jere.chiosso@gmail.com>
2022-11-21 21:20:28 -0300

    initial search bar stlyling

--- frontend/src/components/SearchBar/SearchBarView/SearchBarView.js
+++ frontend/src/components/SearchBar/SearchBarView/SearchBarView.js
@@ -1,17 +1,31 @@
 import './SearchBarView.scss'
 
+import imageLogo from  '../../../assets/Logo_ML.png'
+import imageSearch from '../../../assets/ic_Search.png'
+
 function SearchBarView({
     onSearch,
     onChange,
     value
 }) {
     return <div className='container'>
-        <input
-            placeholder='Nunca dejes de buscar'
-            onChange={onChange}
-            value={value}
-        />
-        <button onClick={onSearch}>Search!</button>
+        <div className='content'>
+            <img src={imageLogo}/>
+            <div className='search-bar'>
+                <input
+                    className='search-bar__input'
+                    placeholder='Nunca dejes de buscar'
+                    onChange={onChange}
+                    value={value}
+                />
+                <button
+                    onClick={onSearch}
+                    className='search-bar__action'
+                >
+                    <img src={imageSearch}/>
+                </button>
+            </div>
+        </div>
     </div>
 }
 
